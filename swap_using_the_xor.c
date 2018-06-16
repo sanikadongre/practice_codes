150a5a175fd510c1cae383430f3fd208bc57de3f
sanikadongre <sado9543@colorado.edu>
2018-06-15 19:09:22 -0600

    adding codes

---
+++ swap_using_the_xor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+
+	int8_t num1=-10, num2= -5;
+	num1 = num1 ^ num2;
+	num2 = num1 ^ num2;
+	num1 = num1 ^ num2;
+	printf("After swapping: x=%d, y=%d\n",x,y);
+        
+
+}
