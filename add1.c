32b6c86eacb7051e1243a3395bb0dc697eb6e46a
Sanika Dongre <sado9543@colorado.edu>
2018-06-19 23:08:21 -0600

    code

---
+++ add1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <stdint.h>
+
+ 
+
+int16_t add_1(int16_t num)
+
+{
+   return (-(~num));
+
+}
+ 
+
+
+
+void main()
+
+{
+  
+printf("%d\n", addOne(-15));
+ 
+printf("%d", addOne(15));
+  
+
+}
