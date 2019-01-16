d5b7a58e936ad0cbb23e0f614ced411ee8eec971
Sanika Dongre <sado9543@colorado.edu>
2019-01-16 06:35:42 -0700

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,36 @@
+def mergesort(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R=  arr[mid:]
+        mergesort(L)
+        mergesort(R)
+        i=j=k=0
+        while i< len(L) and j< len(R):
+            if(L[i] < R[j]):
+                arr[k] = L[i]
+                i+=1
+            else:
+                arr[k] = R[j]
+                j+=1
+            k+=1
+        while i< len(L):
+            arr[k] = L[i]
+            i+=1
+            k+=1
+        while j<len(R):
+            arr[k] = R[j]
+            j+=1
+            k+=1
+def printlist(arr):
+    for i in range(len(arr)):
+        print(arr[i], end = " ")
+    print()
+    
+if __name__ == '__main__':
+    arr=[12,87,6,56,4,1]
+    print ("Given array is", end="\n")  
+    printlist(arr) 
+    mergesort(arr) 
+    print("Sorted array is: ", end="\n") 
+    printlist(arr)
