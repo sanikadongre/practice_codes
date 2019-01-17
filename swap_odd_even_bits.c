d09f9bd2b2780787f53c48601d6ab673a5abc84f
Sanika Dongre <sado9543@colorado.edu>
2019-01-17 03:33:30 -0700

    Create swap_odd_even_bits.c

---
+++ swap_odd_even_bits.c
@@ -0,0 +1,16 @@
+unsigned int swap_bits(unsigned int x)
+{
+   unsigned int even_bits = x & 0xAAAAAAAA;
+   unsigned int odd_bits = x & 0x55555555;
+   even_bits >>= 1;
+   odd_bits <<= 1;
+   return (even_bits|odd_bits);
+   
+   }
+   
+   int main()
+   {
+    unsigned int x = 23;
+    printf("%u", swap_bits(x));
+    return 0;
+    }
