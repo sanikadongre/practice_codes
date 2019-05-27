67b9475e1a51bdb7c2bee76174c2388b55b4444d
Sanika Dongre <sado9543@colorado.edu>
2019-05-27 17:39:47 -0600

    Create hackerrank_challenge_reverse_list.c

---
+++ hackerrank_challenge_reverse_list.c
@@ -0,0 +1,21 @@
+SinglyLinkedListNode
+{
+      int data;
+      SinglyLinkedListNode* next;
+ };
+ SinglyLinkedListNode* reverse(SinglyLinkedListNode* head)
+ {
+      SinglyLinkedListNode* prev = NULL;
+      SinglyLinkedListNode* current = head;
+      SinglyLinkedListNode* next = NULL;
+      while(current!=NULL)
+      {
+          next=current->next;
+          current->next=prev;
+          prev=current;
+          current=next;
+         
+       }
+       head = prev;
+       return head;
+}
